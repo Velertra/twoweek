2e39d415d4142c31114581a06aec48333daacf6d
Velertra <75776124+Velertra@users.noreply.github.com>
2023-09-25 21:00:23 -0700

    updated w/ code editor -eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./api.js";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../src/react.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./react.js";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
