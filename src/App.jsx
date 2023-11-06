a17e0b5506ec89f649d045d7c42c3a940a8c4618
Velertra <75776124+Velertra@users.noreply.github.com>
2023-11-06 07:48:51 -0700

    fix vercel and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import '../src/styles/App.css'
 import FriendDiv from './components/FriendPlayersPage'
 import SearchBar from './components/SearchBar'
--- src/components/DisplayImages.jsx
+++ src/components/DisplayImages.jsx
@@ -4,19 +4,19 @@ export default function IconImages({stats}){
         <>
             <div className={'trophy-case'}>
                 <img
-                    src="/1BE513043_bg.png"
+                    src="/images/1BE513043_bg.png"
                     alt="mancakeboy"
                 />
                 <img
-                    src="/AE8A13020_bg.png"
+                    src="/images/AE8A13020_bg.png"
                     alt="chun-li"
                 />
                 <img
-                    src="/hotdogskin.jpg"
+                    src="/images/hotdogskin.jpg"
                     alt="hotdogman"
                 />
                 <img
-                    src="/i65d3uf90ve91.jpg"
+                    src="/images/i65d3uf90ve91.jpg"
                     alt="antman"
                 />
             </div> 
