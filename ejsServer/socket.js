73fc5db159fd8f15ab0fa351a0ec5d6af9640683
Velertra <75776124+Velertra@users.noreply.github.com>
2024-02-17 22:56:24 -0700

    took off cors for local enviornment

--- ejsServer/socket.js
+++ ejsServer/socket.js
@@ -1,13 +1,13 @@
 var { Server } = require("socket.io");
 require("dotenv").config();
 
-const origin = process.env.PROD_ENV === 'production' ? "https://twoweek.vercel.app/" : "http://localhost:5173";
+//const origin = process.env.PROD_ENV === 'production' ? "https://twoweek.vercel.app/" : "http://localhost:5173";
 
-const io = new Server({
+const io = new Server(/* {
     cors: {
       origin: origin
     }
     
-  });
+  } */);
 
 module.exports = io;
