f98dfcf0ee614a422f95cec8d30279e72498f56d
Velertra <75776124+Velertra@users.noreply.github.com>
2024-02-18 12:22:01 -0700

    client side socket connection test

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_NODE === 'production' ? import.meta.env.VITE_SOCKET_PORT : 'http://localhost:4000';
+const URL = import.meta.env.VITE_NODE === 'production' ? 'testing123' : 'http://localhost:4000';
 
 export const socket = io(URL);
